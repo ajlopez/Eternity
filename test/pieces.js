c4eb8081e8f950afef6590f57d2b3a23824790b0
ajlopez <webmaster@ajlopez.com>
2013-05-19 18:09:40 -0300

    match piece, refactor/redesign

--- test/pieces.js
+++ test/pieces.js
@@ -14,13 +14,13 @@ var height = puzzle.getHeight();
 for (var x = 0; x < width; x++)
     for (var y = 0; y < height; y++) {
         if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-            assert.equal(puzzle.acceptPiece(x, y, [0, 0, 0, 0]), false);
+            assert.equal(puzzle.match(x, y, [0, 0, 0, 0]), null);
         else
-            assert.ok(puzzle.acceptPiece(x, y, [0, 0, 0, 0]));
+            assert.ok(puzzle.match(x, y, [0, 0, 0, 0]));
     }
 
 // accepts four color piece in inner area
 
 for (var x = 2; x < width - 2; x++)
     for (var y = 2; y < height - 2; y++)
-        assert.ok(puzzle.acceptPiece(x, y, [1, 2, 3, 4]));
+        assert.ok(puzzle.match(x, y, [1, 2, 3, 4]));

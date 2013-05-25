99bde4e6a4ae30688539a9271db92e04fde832bc
ajlopez <webmaster@ajlopez.com>
2013-05-25 18:31:13 -0300

    Solve with callback

--- test/solve.js
+++ test/solve.js
@@ -13,3 +13,24 @@ var puzzle = eternity.createPuzzle(4, 4);
 assert.ok(puzzle.solve(pieces));
 assert.ok(puzzle.isValid());
 
+// generates 5x5 with 8 colors and gray
+    
+var initial = eternity.createPuzzle(5, 5);
+initial.generate(8);
+var pieces = initial.getPieces();
+
+var puzzle = eternity.createPuzzle(5, 5);
+
+assert.ok(puzzle.solve(pieces));
+assert.ok(puzzle.isValid());
+
+// generates 6x6 with 12 colors and gray
+    
+var initial = eternity.createPuzzle(6, 6);
+initial.generate(12);
+var pieces = initial.getPieces();
+
+var puzzle = eternity.createPuzzle(6, 6);
+
+assert.ok(puzzle.solve(pieces));
+assert.ok(puzzle.isValid());

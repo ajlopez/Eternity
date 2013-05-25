d97c0b81ac833cfb87cfe443312eaeac29d67ba4
ajlopez <webmaster@ajlopez.com>
2013-05-25 18:26:25 -0300

    First solve with test

--- test/solve.js
+++ test/solve.js
@@ -2,11 +2,14 @@
 var eternity = require('../'),
     assert = require('assert');
     
-// generates 5x5 with 6 colors and gray
+// generates 4x4 with 6 colors and gray
     
-var initial = eternity.createPuzzle(5, 5);
+var initial = eternity.createPuzzle(4, 4);
 initial.generate(6);
+var pieces = initial.getPieces();
 
-var puzzle = eternity.createPuzzle(5, 5);
+var puzzle = eternity.createPuzzle(4, 4);
 
-var pieces = puzzle.getPieces();
+assert.ok(puzzle.solve(pieces));
+assert.ok(puzzle.isValid());
+

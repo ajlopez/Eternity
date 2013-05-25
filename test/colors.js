dbf1a85abf4e11051b659ea39e5586986a52b4b9
ajlopez <webmaster@ajlopez.com>
2013-05-25 16:15:20 -0300

    generate and isValid with tests

--- test/colors.js
+++ test/colors.js
@@ -2,8 +2,55 @@
 var eternity = require('../'),
     assert = require('assert');
     
-var puzzle = eternity.createPuzzle();
+// generates 3x3 with 2 colors and gray
+    
+var puzzle = eternity.createPuzzle(3, 3);
+puzzle.generate(2);
+
+var pieces = puzzle.getPieces();
+
+assert.ok(pieces);
+assert.ok(Array.isArray(pieces));
+assert.equal(pieces.length, 3 * 3);
+
+// it is a valid solution
+
+assert.ok(puzzle.isValid());
+    
+// generates 4x4 with 3 colors and gray
+    
+var puzzle = eternity.createPuzzle(4, 4);
+puzzle.generate(3);
+
+var pieces = puzzle.getPieces();
+
+assert.ok(pieces);
+assert.ok(Array.isArray(pieces));
+assert.equal(pieces.length, 4 * 4);
+
+// it is a valid solution
+
+assert.ok(puzzle.isValid());
+    
+// generates 5x5 with 6 colors and gray
+    
+var puzzle = eternity.createPuzzle(5, 5);
 puzzle.generate(6);
+
+var pieces = puzzle.getPieces();
+
+assert.ok(pieces);
+assert.ok(Array.isArray(pieces));
+assert.equal(pieces.length, 5 * 5);
+
+// it is a valid solution
+
+assert.ok(puzzle.isValid());
+    
+// generates 18x18 with 10 colors and gray
+    
+var puzzle = eternity.createPuzzle();
+puzzle.generate(10);
 
 var pieces = puzzle.getPieces();
 
@@ -11,3 +58,6 @@ assert.ok(pieces);
 assert.ok(Array.isArray(pieces));
 assert.equal(pieces.length, 18 * 18);
 
+// it is a valid solution
+
+assert.ok(puzzle.isValid());
